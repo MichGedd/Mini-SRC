2c81af3314610efb5d6fb83169cdb5646239fb6e
Michael Geddes <michaelgeddes555@gmail.com>
2022-01-26 23:00:49 -0500

    ADD, SUB, AND, OR, NOT, and NEG all work on ALU

--- system/alu_32.v
+++ system/alu_32.v
@@ -1,4 +1,4 @@
-module alu_32(input [31:0] in_a, input [31:0] in_b, input [31:0] in_opcode, output reg [31:0] out_result);
+module alu_32(input [31:0] in_a, input [31:0] in_b, input [3:0] in_opcode, output reg [31:0] out_result);
 
 	// ALU opcodes
 	/*
@@ -19,44 +19,96 @@ module alu_32(input [31:0] in_a, input [31:0] in_b, input [31:0] in_opcode, outp
 	wire [31:0] w_and_32;
 	wire [31:0] w_or_32;
 	wire [31:0] w_not_32;
+	wire [31:0] w_adder_sum_out;
+	wire w_adder_carry_out;
 	
 	reg [31:0] r_a_pre_process;
 	reg [31:0] r_b_pre_process;
 	reg [31:0] r_a_inverted;
+	reg r_adder_carry_in;
 	
 	assign w_and_32 = in_a & in_b;
 	assign w_or_32 = in_a | in_b;
-	assign w_not_32 = !r_a_pre_process;
+	assign w_not_32 = ~r_a_pre_process;
+	
+	adder_32 adder(.in_x (r_a_inverted),
+		.in_y (r_b_pre_process),
+		.in_carry (r_adder_carry_in),
+		.out_sum (w_adder_sum_out),
+		.out_carry (w_adder_carry_out));
 	
 	always @(*) begin
-		case(in_opcode)
+		case(in_opcode)  // Pre-process step before addition/not
 			4'b1010,
-			4'b1011 : r_b_pre_process = 0;
-			4'b0001 : begin
+			4'b1011 : begin
+				r_b_pre_process = 0;  // If op code is NOT or INV set b to 0
+				r_a_pre_process = in_a;
+			end
+			4'b0001 : begin  // If op code is SUB swap A and B
 				r_a_pre_process = in_b;
 				r_b_pre_process = in_a;
 			end
-			
-			default : begin
+			default : begin  // Default is pass values through
 				r_a_pre_process = in_a;
 				r_b_pre_process = in_b;
 			end
 		endcase
 		
-		case(in_opcode)
-			4'b0001,
+		case(in_opcode)  // Whether to invert A
+			4'b0001,  // If op code is SUB, NOT, or INV, do a bitwise NOT to a
 			4'b1010,
 			4'b1011 : r_a_inverted = w_not_32;
-			default : r_a_inverted = r_a_pre_process;
+			default : r_a_inverted = r_a_pre_process;  // Default is pass through values
 		endcase
 		
 		case(in_opcode)
-			4'b0110 : out_result = w_and_32;
-			4'b0111 : out_result = w_or_32;
+			4'b0001,
+			4'b1010 : r_adder_carry_in = 1;
+			default : r_adder_carry_in = 0;
+		endcase
+		
+		case(in_opcode)  // Final ALU output
+			4'b0000,
+			4'b0001,
+			4'b1010,
+			4'b1011 : out_result = w_adder_sum_out;  // ADD, SUB, NOT, INV
+			4'b0110 : out_result = w_and_32;  // AND
+			4'b0111 : out_result = w_or_32;  // OR
 			default : out_result = 0;
 		endcase
 	end
 	
+endmodule
+
+// --------------------------
+
+module alu_32_tb;
+	reg [31:0] in_a, in_b;
+	reg [3:0] opcode;
+	wire [31:0] alu_out;
 	
+	parameter delay = 10;
+	
+	alu_32 DUT(.in_a (in_a),
+		.in_b (in_b),
+		.in_opcode (opcode),
+		.out_result (alu_out));
+	
+	initial begin
+		in_a = 32'h0; in_b = 32'h0; opcode = 4'b0;
+		#(delay) in_a = 32'hF0F0F0F0; in_b = 32'hABCDABCD; opcode = 4'b1011; // NOT
+		#(delay) in_a = 32'h0; in_b = 32'h0; opcode = 4'b0;
+		#(delay) in_a = 32'h00000001; in_b = 32'hFFFFFFFF; opcode = 4'b1010; // NEG
+		#(delay) in_a = 32'h0; in_b = 32'h0; opcode = 4'b0;
+		#(delay) in_a = 32'hFFFFFFFF; in_b = 32'h0F0F0F0F; opcode = 4'b0110;  // AND
+		#(delay) in_a = 32'h0; in_b = 32'h0; opcode = 4'b0;
+		#(delay) in_a = 32'hF0F0F0F0; in_b = 32'h0F0F0F0F; opcode = 4'b0111;  // OR
+		#(delay) in_a = 32'h0; in_b = 32'h0; opcode = 4'b0;
+		#(delay) in_a = 32'h0000FFFF; in_b = 32'h00000001; opcode = 4'b0000;  // ADD
+		#(delay) in_a = 32'h0; in_b = 32'h0; opcode = 4'b0;
+		#(delay) in_a = 32'h0000FFFF; in_b = 32'h000000FF; opcode = 4'b0001;  // SUB
+		#(delay) in_a = 32'h0; in_b = 32'h0; opcode = 4'b0;
+	end
 	
 endmodule
+
---
+++ system/cla_16.v
@@ -0,0 +1,65 @@
+module cla_16(input [15:0] in_x, input [15:0] in_y, input in_carry, output [15:0] out_sum, output out_carry, output out_generate, output out_propogate);
+
+	wire [4:0] w_carry;
+	wire [3:0] w_generate;
+	wire [3:0] w_propogate;
+	
+	assign w_carry[0] = in_carry;
+	assign out_carry = w_carry[4];
+	
+	genvar i;
+	
+	generate
+		for(i = 0; i < 16; i = i + 4) begin: gen_cla_4
+			cla_4 cla(.in_x (in_x[i+3:i]),
+				.in_y (in_y[i+3:i]),
+				.in_carry (w_carry[i/4]),
+				.out_sum (out_sum[i+3:i]),
+				.out_generate (w_generate[i/4]),
+				.out_propogate (w_propogate[i/4]));
+		end
+	endgenerate
+	
+	cla_logic cla(.in_carry (w_carry[0]),
+		.in_generate (w_generate),
+		.in_propogate (w_propogate),
+		.out_carry (w_carry[4:1]),
+		.out_generate (out_generate),
+		.out_propogate (out_propogate));
+
+endmodule
+
+//--------------------------
+
+module cla_16_tb;
+
+	reg [15:0] in_x, in_y;
+	reg in_carry;
+	
+	wire [15:0] out_sum;
+	wire out_generate, out_propogate, out_carry;
+	
+	parameter delay = 10;
+	
+	cla_16 DUT(.in_x (in_x),
+		.in_y (in_y),
+		.in_carry (in_carry),
+		.out_sum (out_sum),
+		.out_carry (out_carry),
+		.out_generate (out_generate),
+		.out_propogate (out_propogate));
+		
+	initial begin
+		in_x = 16'h0000; in_y = 16'h0000; in_carry = 0;
+		
+		#(delay) in_x = 16'h0001; in_y = 16'h000F; in_carry = 0; // 1 + 15
+		#(delay) in_x = 16'h0000; in_y = 16'h0000; in_carry = 0; // 0 + 0
+		#(delay) in_x = 16'h0001; in_y = 16'h000F; in_carry = 1; // 1 + 15 + 1
+		#(delay) in_x = 16'h0000; in_y = 16'h0000; in_carry = 0; // 0 + 0
+		#(delay) in_x = 16'hFFFF; in_y = 16'h0002; in_carry = 0; // -1 + 2
+		#(delay) in_x = 16'h0000; in_y = 16'h0000; in_carry = 0; // 0 + 0
+		#(delay) in_x = 16'hFFFF; in_y = 16'h0002; in_carry = 1; // -1 + 2 + 1
+		#100;
+	end
+
+endmodule
