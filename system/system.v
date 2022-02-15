72f08b827b42bfc117842071be8b23d00c5b5747
Michael Geddes <michaelgeddes555@gmail.com>
2022-02-15 15:51:53 -0500

    Added memory block
    - Added memory using MegaWizard plugin
    - Added memory initialization file
    - Updated datapath for new memory inputs
    - Removed datapath testbenches

--- system/system.v
+++ system/system.v
@@ -1,5 +1,192 @@
-module system(input a, output b);
+module system(input clk,
+	input [3:0] in_regfile_location,  // Location to read from
+	input [3:0] in_alu_opcode, // ALU opcode
+	input in_reg_clear,  // Clear all registers
+	input in_mdr_select, // This selects whether to read from bus or memory. 0 for bus, 1 for memory
+	input in_inc_pc,
+	// Read signals
+	input in_regfile_read,
+	input in_hi_read,
+	input in_lo_read,
+	input in_z_hi_read,
+	input in_z_lo_read,
+	input in_pc_read,
+	input in_mdr_read,
+	input in_inport_read,
+	input in_c_read,
+	input in_mem_read,
+	// Write signals
+	input in_regfile_write,
+	input in_hi_write,
+	input in_lo_write,
+	input in_z_write,
+	input in_pc_write,
+	input in_mdr_write,
+	input in_ir_write,
+	input in_y_write,
+	input in_mar_write,
+	input in_mem_write,
+	output [31:0] out_bus);  // Maybe remove this later
 
-	assign b = !a;
+	wire [31:0] w_mem_address;
+	wire [31:0] w_mdr_data;
+	wire [31:0] w_mem_data;
+
+	datapath path (.clk (clk),
+		.in_regfile_location (in_regfile_location),
+		.in_alu_opcode (in_alu_opcode),
+		.in_mem_data (w_mem_data),
+		.in_reg_clear (in_reg_clear),
+		.in_inc_pc (in_inc_pc),
+		.in_mdr_select (in_mdr_select),
+		.in_regfile_read (in_regfile_read),
+		.in_hi_read (in_hi_read),
+		.in_lo_read (in_lo_read),
+		.in_z_hi_read (in_z_hi_read),
+		.in_z_lo_read (in_z_lo_read),
+		.in_pc_read (in_pc_read),
+		.in_mdr_read (in_mdr_read),
+		.in_inport_read (in_inport_read),
+		.in_c_read (in_c_read),
+		.in_regfile_write (in_regfile_write),
+		.in_hi_write (in_hi_write),
+		.in_lo_write (in_lo_write),
+		.in_z_write (in_z_write),
+		.in_pc_write (in_pc_write),
+		.in_mdr_write (in_mdr_write),
+		.in_ir_write (in_ir_write),
+		.in_y_write (in_y_write),
+		.in_mar_write (in_mar_write),
+		.out_bus (out_bus),  // Maybe change this later
+		.out_mdr (w_mdr_data),
+		.out_mar (w_mem_address));
+	
+	memory RAM (.clock (clk),
+		.address (w_mem_address),
+		.data (w_mdr_data),
+		.rden (in_mem_read),
+		.wren (in_mem_write),
+		.q (w_mem_data));
+
+endmodule
+
+//---------------------------
+
+module system_tb;
+	reg clk, reg_clear, mdr_select, inc_pc;
+	reg regfile_read, hi_read, lo_read, z_hi_read, z_lo_read, pc_read, mdr_read, inport_read, c_read, mem_read;
+	reg regfile_write, hi_write, lo_write, z_write, pc_write, mdr_write, ir_write, y_write, mar_write, mem_write;
+	reg [3:0] regfile_location, alu_opcode;
+	
+	wire[31:0] bus;
+	
+	system DUT (.clk (clk),
+		.in_regfile_location (regfile_location),
+		.in_alu_opcode (alu_opcode),
+		.in_reg_clear (reg_clear),
+		.in_inc_pc (inc_pc),
+		.in_mdr_select (mdr_select),
+		.in_regfile_read (regfile_read),
+		.in_hi_read (hi_read),
+		.in_lo_read (lo_read),
+		.in_z_hi_read (z_hi_read),
+		.in_z_lo_read (z_lo_read),
+		.in_pc_read (pc_read),
+		.in_mdr_read (mdr_read),
+		.in_inport_read (inport_read),
+		.in_c_read (c_read),
+		.in_mem_read (mem_read),
+		.in_regfile_write (regfile_write),
+		.in_hi_write (hi_write),
+		.in_lo_write (lo_write),
+		.in_z_write (z_write),
+		.in_pc_write (pc_write),
+		.in_mdr_write (mdr_write),
+		.in_ir_write (ir_write),
+		.in_y_write (y_write),
+		.in_mar_write (mar_write),
+		.in_mem_write (mem_write),
+		.out_bus (bus));
+	
+	// Set up clock
+	initial begin
+		clk = 0;
+		forever #10 clk = ~clk;
+	end
+	
+	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011,
+		Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111,
+		T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100, T6 = 4'b1101;
+	
+	reg[3:0] state = Default;
+	
+	always @(posedge clk) begin
+		case(state)
+			Default : state = T0;
+			T0 : state = T1;
+			T1 : state = T2;
+			T2 : state = T3;
+			T3 : state = T4;
+			T4 : state = T5;
+			T5 : state = T6;
+		endcase
+	end
+	
+	// Reset Variables Function
+	task reset_read_write_signals();
+		begin
+		regfile_read = 0;
+		hi_read = 0;
+		lo_read = 0;
+		z_hi_read = 0;
+		z_lo_read = 0;
+		pc_read = 0;
+		mdr_read = 0;
+		inport_read = 0;
+		c_read = 0;
+		mem_read = 0;
+		regfile_write = 0;
+		hi_write = 0;
+		lo_write = 0;
+		pc_write = 0;
+		mdr_write = 0;
+		ir_write = 0;
+		y_write = 0;
+		z_write = 0;
+		mar_write = 0;
+		mem_write = 0;
+		end
+	endtask
+	
+	always @(state) begin
+		case(state)
+			Default : begin
+				reg_clear <= 1;
+				alu_opcode <= 0;
+				regfile_location <= 0;
+				mdr_select <= 0;
+				inc_pc <= 0;
+			end
+			T0 : begin
+				reset_read_write_signals();
+				pc_read <= 1;  // Put PC on the bus
+				mar_write <= 1;  // Write bus to MAR
+				inc_pc <= 1;  // Increment PC
+				pc_write <= 1;  // Increment PC
+			end
+			T1 : begin  // Memory has registered inports, so theres a 1 cycle delay between mem_read and getting the output
+				reset_read_write_signals();
+				inc_pc <= 0;  // Stop incrementing PC
+				mem_read <= 1;
+			end
+			T2 : begin
+				reset_read_write_signals();
+				mdr_write <= 1;  // Write memory to MDR
+				mdr_select <= 1;  // Write memory to MDR
+				mdr_read <= 1;  // Put MDR onto the bus
+				ir_write <= 1;  // Write bus to IR
+			end
+		endcase
+	end
 
 endmodule
