ef49232fd439fde182fe1eced9c0cf9e106282bf
Michael Geddes <michaelgeddes555@gmail.com>
2022-03-02 00:03:05 -0500

    Finished tb for branch instructions

--- system/system.v
+++ system/system.v
@@ -1606,7 +1606,7 @@ module system_tb_ori;
 endmodule
 
 module system_tb_brzr;
-	// Set M[0] to 10010 0010 00000000000000000100011
+	// Set M[0] to 10010001000000000000000000100011
 	// Set R2 to a value
 	// Output -> PC = PC + 1 + 35 = 36 (if R2 is set to 0)
 	
@@ -1663,6 +1663,507 @@ module system_tb_brzr;
 		//Do this to instantiate the value in a register -> DUT.path.Y.value_32 = 32'b1;
 		//Do this to acccess a specific register in the regfile (This is reg 0) -> DUT.path.regfile.gen_registers[0].register_inst.value_32 = 32'b1;
 		DUT.path.regfile.gen_registers[2].register_inst.value_32 = 32'd0;
+		clk = 0;
+		forever #10 clk = ~clk;
+	end
+	
+	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011,
+		Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111,
+		T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100, T6 = 4'b1101, T7 = 4'b1110;
+	
+	reg[3:0] state = Default;
+	
+	always @(posedge clk) begin
+		case(state)
+			Default : state = T0;
+			T0 : state = T1;
+			T1 : state = T2;
+			T2 : state = T3;
+			T3 : state = T4;
+			T4 : state = T5;
+			T5 : state = T6;
+		endcase
+	end
+	
+	// Reset Variables Function
+	task reset_read_write_signals();
+		begin
+		regfile_read = 0;
+		hi_read = 0;
+		lo_read = 0;
+		z_hi_read = 0;
+		z_lo_read = 0;
+		pc_read = 0;
+		mdr_read = 0;
+		inport_read = 0;
+		c_read = 0;
+		mem_read = 0;
+		regfile_write = 0;
+		hi_write = 0;
+		lo_write = 0;
+		pc_write = 0;
+		mdr_write = 0;
+		ir_write = 0;
+		y_write = 0;
+		z_write = 0;
+		mar_write = 0;
+		mem_write = 0;
+		
+		gra = 0;
+		grb = 0;
+		grc = 0;
+		ba_read = 0;
+		
+		con_write = 0;
+		end
+	endtask
+	
+	always @(state) begin
+		case(state)
+			Default : begin
+				reg_clear <= 1;
+				alu_opcode <= 0;
+				mdr_select <= 0;
+				inc_pc <= 0;
+				reset_read_write_signals();
+			end
+			T0 : begin
+				reset_read_write_signals();
+				pc_read <= 1;  // Put PC on the bus
+				mar_write <= 1;  // Write bus to MAR
+				inc_pc <= 1;  // Increment PC
+				pc_write <= 1;  // Increment PC
+				mem_read <= 1;  // Read from memory
+			end
+			T1 : begin 
+				reset_read_write_signals();
+				inc_pc <= 0;  // Stop incrementing PC
+				mdr_write <= 1;  // Write memory to MDR
+				mdr_select <= 1;  // Write memory to MDR
+			end
+			T2 : begin
+				reset_read_write_signals();
+				mdr_read <= 1;  // Put MDR onto the bus
+				ir_write <= 1;  // Write bus to IR
+			end
+			T3 : begin
+				reset_read_write_signals();
+				gra <= 1;
+				regfile_read <= 1;
+				con_write <= 1;
+			end
+			T4 : begin
+				reset_read_write_signals();
+				pc_read <= 1;
+				y_write <= 1;
+			end
+			T5 : begin
+				reset_read_write_signals();
+				c_read <= 1;
+				alu_opcode <= 4'd0000;
+				z_write <= 1;
+			end
+			T6 : begin
+				reset_read_write_signals();
+				z_lo_read <= 1;
+				pc_write <= branch;
+			end
+		endcase
+	end
+endmodule
+
+module system_tb_brnz;
+	// Set M[0] to 10010001000010000000000000100011
+	// Set R2 to a value
+	// Output -> PC = PC + 1 + 35 = 36 (if R2 is not zero)
+	
+	reg clk, reg_clear, mdr_select, inc_pc, gra, grb, grc, ba_read, con_write;
+	reg regfile_read, hi_read, lo_read, z_hi_read, z_lo_read, pc_read, mdr_read, inport_read, c_read, mem_read;
+	reg regfile_write, hi_write, lo_write, z_write, pc_write, mdr_write, ir_write, y_write, mar_write, mem_write;
+	reg [3:0] alu_opcode;
+	
+	reg [31:0] inport_data;
+	reg outport_write;
+	
+	wire[31:0] outport;
+	wire[31:0] bus;
+	wire branch;
+	
+	system DUT (.clk (clk),
+		.in_alu_opcode (alu_opcode),
+		.in_reg_clear (reg_clear),
+		.in_inc_pc (inc_pc),
+		.in_mdr_select (mdr_select),
+		.in_regfile_read (regfile_read),
+		.in_gra (gra),
+		.in_grb (grb),
+		.in_grc (grc),
+		.in_ba_read (ba_read),
+		.in_con_write (con_write),
+		.in_hi_read (hi_read),
+		.in_lo_read (lo_read),
+		.in_z_hi_read (z_hi_read),
+		.in_z_lo_read (z_lo_read),
+		.in_pc_read (pc_read),
+		.in_mdr_read (mdr_read),
+		.in_inport_read (inport_read),
+		.in_c_read (c_read),
+		.in_mem_read (mem_read),
+		.in_regfile_write (regfile_write),
+		.in_hi_write (hi_write),
+		.in_lo_write (lo_write),
+		.in_z_write (z_write),
+		.in_pc_write (pc_write),
+		.in_mdr_write (mdr_write),
+		.in_ir_write (ir_write),
+		.in_y_write (y_write),
+		.in_mar_write (mar_write),
+		.in_mem_write (mem_write),
+		.out_bus (bus),
+		.in_outport_write (outport_write),
+		.in_inport_data (inport_data),
+		.out_outport(outport),
+		.out_branch(branch));
+	
+	// Set up clock
+	initial begin
+		//Do this to instantiate the value in a register -> DUT.path.Y.value_32 = 32'b1;
+		//Do this to acccess a specific register in the regfile (This is reg 0) -> DUT.path.regfile.gen_registers[0].register_inst.value_32 = 32'b1;
+		DUT.path.regfile.gen_registers[2].register_inst.value_32 = 32'd1;
+		clk = 0;
+		forever #10 clk = ~clk;
+	end
+	
+	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011,
+		Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111,
+		T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100, T6 = 4'b1101, T7 = 4'b1110;
+	
+	reg[3:0] state = Default;
+	
+	always @(posedge clk) begin
+		case(state)
+			Default : state = T0;
+			T0 : state = T1;
+			T1 : state = T2;
+			T2 : state = T3;
+			T3 : state = T4;
+			T4 : state = T5;
+			T5 : state = T6;
+		endcase
+	end
+	
+	// Reset Variables Function
+	task reset_read_write_signals();
+		begin
+		regfile_read = 0;
+		hi_read = 0;
+		lo_read = 0;
+		z_hi_read = 0;
+		z_lo_read = 0;
+		pc_read = 0;
+		mdr_read = 0;
+		inport_read = 0;
+		c_read = 0;
+		mem_read = 0;
+		regfile_write = 0;
+		hi_write = 0;
+		lo_write = 0;
+		pc_write = 0;
+		mdr_write = 0;
+		ir_write = 0;
+		y_write = 0;
+		z_write = 0;
+		mar_write = 0;
+		mem_write = 0;
+		
+		gra = 0;
+		grb = 0;
+		grc = 0;
+		ba_read = 0;
+		
+		con_write = 0;
+		end
+	endtask
+	
+	always @(state) begin
+		case(state)
+			Default : begin
+				reg_clear <= 1;
+				alu_opcode <= 0;
+				mdr_select <= 0;
+				inc_pc <= 0;
+				reset_read_write_signals();
+			end
+			T0 : begin
+				reset_read_write_signals();
+				pc_read <= 1;  // Put PC on the bus
+				mar_write <= 1;  // Write bus to MAR
+				inc_pc <= 1;  // Increment PC
+				pc_write <= 1;  // Increment PC
+				mem_read <= 1;  // Read from memory
+			end
+			T1 : begin 
+				reset_read_write_signals();
+				inc_pc <= 0;  // Stop incrementing PC
+				mdr_write <= 1;  // Write memory to MDR
+				mdr_select <= 1;  // Write memory to MDR
+			end
+			T2 : begin
+				reset_read_write_signals();
+				mdr_read <= 1;  // Put MDR onto the bus
+				ir_write <= 1;  // Write bus to IR
+			end
+			T3 : begin
+				reset_read_write_signals();
+				gra <= 1;
+				regfile_read <= 1;
+				con_write <= 1;
+			end
+			T4 : begin
+				reset_read_write_signals();
+				pc_read <= 1;
+				y_write <= 1;
+			end
+			T5 : begin
+				reset_read_write_signals();
+				c_read <= 1;
+				alu_opcode <= 4'd0000;
+				z_write <= 1;
+			end
+			T6 : begin
+				reset_read_write_signals();
+				z_lo_read <= 1;
+				pc_write <= branch;
+			end
+		endcase
+	end
+endmodule
+
+module system_tb_brpl;
+	// Set M[0] to 10010001000100000000000000100011
+	// Set R2 to a value
+	// Output -> PC = PC + 1 + 35 = 36 (if R2 is >= 0)
+	
+	reg clk, reg_clear, mdr_select, inc_pc, gra, grb, grc, ba_read, con_write;
+	reg regfile_read, hi_read, lo_read, z_hi_read, z_lo_read, pc_read, mdr_read, inport_read, c_read, mem_read;
+	reg regfile_write, hi_write, lo_write, z_write, pc_write, mdr_write, ir_write, y_write, mar_write, mem_write;
+	reg [3:0] alu_opcode;
+	
+	reg [31:0] inport_data;
+	reg outport_write;
+	
+	wire[31:0] outport;
+	wire[31:0] bus;
+	wire branch;
+	
+	system DUT (.clk (clk),
+		.in_alu_opcode (alu_opcode),
+		.in_reg_clear (reg_clear),
+		.in_inc_pc (inc_pc),
+		.in_mdr_select (mdr_select),
+		.in_regfile_read (regfile_read),
+		.in_gra (gra),
+		.in_grb (grb),
+		.in_grc (grc),
+		.in_ba_read (ba_read),
+		.in_con_write (con_write),
+		.in_hi_read (hi_read),
+		.in_lo_read (lo_read),
+		.in_z_hi_read (z_hi_read),
+		.in_z_lo_read (z_lo_read),
+		.in_pc_read (pc_read),
+		.in_mdr_read (mdr_read),
+		.in_inport_read (inport_read),
+		.in_c_read (c_read),
+		.in_mem_read (mem_read),
+		.in_regfile_write (regfile_write),
+		.in_hi_write (hi_write),
+		.in_lo_write (lo_write),
+		.in_z_write (z_write),
+		.in_pc_write (pc_write),
+		.in_mdr_write (mdr_write),
+		.in_ir_write (ir_write),
+		.in_y_write (y_write),
+		.in_mar_write (mar_write),
+		.in_mem_write (mem_write),
+		.out_bus (bus),
+		.in_outport_write (outport_write),
+		.in_inport_data (inport_data),
+		.out_outport(outport),
+		.out_branch(branch));
+	
+	// Set up clock
+	initial begin
+		//Do this to instantiate the value in a register -> DUT.path.Y.value_32 = 32'b1;
+		//Do this to acccess a specific register in the regfile (This is reg 0) -> DUT.path.regfile.gen_registers[0].register_inst.value_32 = 32'b1;
+		DUT.path.regfile.gen_registers[2].register_inst.value_32 = 32'h1;
+		clk = 0;
+		forever #10 clk = ~clk;
+	end
+	
+	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011,
+		Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111,
+		T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100, T6 = 4'b1101, T7 = 4'b1110;
+	
+	reg[3:0] state = Default;
+	
+	always @(posedge clk) begin
+		case(state)
+			Default : state = T0;
+			T0 : state = T1;
+			T1 : state = T2;
+			T2 : state = T3;
+			T3 : state = T4;
+			T4 : state = T5;
+			T5 : state = T6;
+		endcase
+	end
+	
+	// Reset Variables Function
+	task reset_read_write_signals();
+		begin
+		regfile_read = 0;
+		hi_read = 0;
+		lo_read = 0;
+		z_hi_read = 0;
+		z_lo_read = 0;
+		pc_read = 0;
+		mdr_read = 0;
+		inport_read = 0;
+		c_read = 0;
+		mem_read = 0;
+		regfile_write = 0;
+		hi_write = 0;
+		lo_write = 0;
+		pc_write = 0;
+		mdr_write = 0;
+		ir_write = 0;
+		y_write = 0;
+		z_write = 0;
+		mar_write = 0;
+		mem_write = 0;
+		
+		gra = 0;
+		grb = 0;
+		grc = 0;
+		ba_read = 0;
+		
+		con_write = 0;
+		end
+	endtask
+	
+	always @(state) begin
+		case(state)
+			Default : begin
+				reg_clear <= 1;
+				alu_opcode <= 0;
+				mdr_select <= 0;
+				inc_pc <= 0;
+				reset_read_write_signals();
+			end
+			T0 : begin
+				reset_read_write_signals();
+				pc_read <= 1;  // Put PC on the bus
+				mar_write <= 1;  // Write bus to MAR
+				inc_pc <= 1;  // Increment PC
+				pc_write <= 1;  // Increment PC
+				mem_read <= 1;  // Read from memory
+			end
+			T1 : begin 
+				reset_read_write_signals();
+				inc_pc <= 0;  // Stop incrementing PC
+				mdr_write <= 1;  // Write memory to MDR
+				mdr_select <= 1;  // Write memory to MDR
+			end
+			T2 : begin
+				reset_read_write_signals();
+				mdr_read <= 1;  // Put MDR onto the bus
+				ir_write <= 1;  // Write bus to IR
+			end
+			T3 : begin
+				reset_read_write_signals();
+				gra <= 1;
+				regfile_read <= 1;
+				con_write <= 1;
+			end
+			T4 : begin
+				reset_read_write_signals();
+				pc_read <= 1;
+				y_write <= 1;
+			end
+			T5 : begin
+				reset_read_write_signals();
+				c_read <= 1;
+				alu_opcode <= 4'd0000;
+				z_write <= 1;
+			end
+			T6 : begin
+				reset_read_write_signals();
+				z_lo_read <= 1;
+				pc_write <= branch;
+			end
+		endcase
+	end
+endmodule
+
+module system_tb_brmi;
+	// Set M[0] to 10010001000110000000000000100011
+	// Set R2 to a value
+	// Output -> PC = PC + 1 + 35 = 36 (if R2 is < 0)
+	
+	reg clk, reg_clear, mdr_select, inc_pc, gra, grb, grc, ba_read, con_write;
+	reg regfile_read, hi_read, lo_read, z_hi_read, z_lo_read, pc_read, mdr_read, inport_read, c_read, mem_read;
+	reg regfile_write, hi_write, lo_write, z_write, pc_write, mdr_write, ir_write, y_write, mar_write, mem_write;
+	reg [3:0] alu_opcode;
+	
+	reg [31:0] inport_data;
+	reg outport_write;
+	
+	wire[31:0] outport;
+	wire[31:0] bus;
+	wire branch;
+	
+	system DUT (.clk (clk),
+		.in_alu_opcode (alu_opcode),
+		.in_reg_clear (reg_clear),
+		.in_inc_pc (inc_pc),
+		.in_mdr_select (mdr_select),
+		.in_regfile_read (regfile_read),
+		.in_gra (gra),
+		.in_grb (grb),
+		.in_grc (grc),
+		.in_ba_read (ba_read),
+		.in_con_write (con_write),
+		.in_hi_read (hi_read),
+		.in_lo_read (lo_read),
+		.in_z_hi_read (z_hi_read),
+		.in_z_lo_read (z_lo_read),
+		.in_pc_read (pc_read),
+		.in_mdr_read (mdr_read),
+		.in_inport_read (inport_read),
+		.in_c_read (c_read),
+		.in_mem_read (mem_read),
+		.in_regfile_write (regfile_write),
+		.in_hi_write (hi_write),
+		.in_lo_write (lo_write),
+		.in_z_write (z_write),
+		.in_pc_write (pc_write),
+		.in_mdr_write (mdr_write),
+		.in_ir_write (ir_write),
+		.in_y_write (y_write),
+		.in_mar_write (mar_write),
+		.in_mem_write (mem_write),
+		.out_bus (bus),
+		.in_outport_write (outport_write),
+		.in_inport_data (inport_data),
+		.out_outport(outport),
+		.out_branch(branch));
+	
+	// Set up clock
+	initial begin
+		//Do this to instantiate the value in a register -> DUT.path.Y.value_32 = 32'b1;
+		//Do this to acccess a specific register in the regfile (This is reg 0) -> DUT.path.regfile.gen_registers[0].register_inst.value_32 = 32'b1;
+		DUT.path.regfile.gen_registers[2].register_inst.value_32 = 32'hFFFFFFFF;
 		clk = 0;
 		forever #10 clk = ~clk;
 	end
